ea8cb50bc9231cdd9a22b560175a487c01b8aeb8
mariam <info.mariam2002@gmail.com>
2021-08-31 13:46:59 +0430

    add new change to phonBook project

--- scripts/Layout.js
+++ scripts/Layout.js
@@ -2,9 +2,9 @@
 
 	var $navToggle = $('.nav-toggle');
 
-	
 	$navToggle.on('click', function(e){
 		$('body').toggleClass('show-menu');
+		$('.nav').toggleClass('show-menu');
 		$(this).toggleClass('is-active');
 	})	
 

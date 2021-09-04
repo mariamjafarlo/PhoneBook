e895552038c2feb89dc4545bdf2bb5c4dbfbe85f
mariam <info.mariam2002@gmail.com>
2021-09-04 17:01:15 +0430

    add table to phonebook project

---
+++ scripts/ContactGroup.js
@@ -0,0 +1,37 @@
+let inputName = document.querySelector("#input-name");
+let btn = document.querySelector(".save");
+let table = document.querySelector("#table");
+let n = 0;
+
+btn.addEventListener("click", (e) => {
+  e.preventDefault();
+  const groupName = inputName.value.trim();
+  if (groupName.length > 0) {
+
+    table.classList.add('active')
+
+    const tr = document.createElement("tr");
+    const tdId = document.createElement("td");
+
+    tdId.setAttribute("class", "id-number");
+    tdId.textContent = n + 1;
+
+    const tdName = document.createElement("td");
+    tdName.textContent = groupName;
+
+    const tdBtn = document.createElement("td");
+    tdBtn.innerHTML = `
+    <span class="material-icons fa fa-pencil"></span>
+    <span class="material-icons fa fa-trash"></span>`
+    ;
+
+    tr.appendChild(tdId);
+    tr.appendChild(tdName);
+    tr.appendChild(tdBtn);
+
+    table.appendChild(tr);
+    n++;
+  } else {
+    alert("نام گروه را وارد کنید");
+  }
+});
